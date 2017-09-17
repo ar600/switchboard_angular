7867dd160507bbb3201c908922e4f3a69569a9a2
alireza <alex.80.ra@gmail.com>
2017-09-17 14:30:11 -0700

    initial commit on Switchboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  switches : boolean[]=[true,true,true,true,true];
+  changeSwitch = function(idx){
+    this.switches[idx] = !this.switches[idx];
+
+
+  }
 }
